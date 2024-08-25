7dca625ffb22e249f39b2c3d9c0199040ad2307b
Nearezz <biswasindranil019@gmail.com>
2024-08-25 13:02:13 -0400

    Initial Commit

---
+++ Calculator_Project/Calculator_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_ProjectApp.swift
+//  Calculator_Project
+//
+//  Created by biswas on 8/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
